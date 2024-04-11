3f12e9896078261667884254f477ccb8f8fb423c
3FANG <d.hasimikov@mail.ru>
2024-04-11 14:58:08 +0300

    Add label to TaskFilter.labels and translate

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -7,7 +7,7 @@ from task_manager.labels.models import Labels
 
 
 class TasksFilter(django_filters.FilterSet):
-    labels = django_filters.ModelChoiceFilter(queryset=Labels.objects.all())
+    labels = django_filters.ModelChoiceFilter(queryset=Labels.objects.all(), label=_("Label"))
     self_tasks = django_filters.BooleanFilter(
         label=_("Only your tasks"),
         method="get_user_tasks",
